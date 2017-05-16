8df8510a492b4115d5a6745c12f9b49980d979f3
Samuel Dressel <fullmanmode@gmail.com>
2017-05-16 10:06:27 +0200

    Basics

---
+++ Eisenbahnsimulator/MainFrame.cpp
@@ -0,0 +1,15 @@
+#include "MainFrame.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Eisenbahnsimulator::MainFrame form;
+	Application::Run(%form);
+}
---
+++ Eisenbahnsimulator/MainFrame.h
@@ -0,0 +1,66 @@
+#pragma once
+
+namespace Eisenbahnsimulator {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Zusammenfassung für MainFrame
+	/// </summary>
+	public ref class MainFrame : public System::Windows::Forms::Form
+	{
+	public:
+		MainFrame(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Konstruktorcode hier hinzufügen.
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		~MainFrame()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// MainFrame
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(620, 478);
+			this->Name = L"MainFrame";
+			this->ShowIcon = false;
+			this->Text = L"Eisenbahnsimulator";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
