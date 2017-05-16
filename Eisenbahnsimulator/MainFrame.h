435f95378c0657b496cc5d545df72b51939f2f70
Samuel Dressel <fullmanmode@gmail.com>
2017-05-16 10:33:49 +0200

    Basic GUI

--- Eisenbahnsimulator/MainFrame.h
+++ Eisenbahnsimulator/MainFrame.h
@@ -10,7 +10,7 @@ namespace Eisenbahnsimulator {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Zusammenfassung für MainFrame
+	/// Summary for MyForm
 	/// </summary>
 	public ref class MainFrame : public System::Windows::Forms::Form
 	{
@@ -19,13 +19,13 @@ namespace Eisenbahnsimulator {
 		{
 			InitializeComponent();
 			//
-			//TODO: Konstruktorcode hier hinzufügen.
+			//TODO: Add the constructor code here
 			//
 		}
 
 	protected:
 		/// <summary>
-		/// Verwendete Ressourcen bereinigen.
+		/// Clean up any resources being used.
 		/// </summary>
 		~MainFrame()
 		{
@@ -34,33 +34,323 @@ namespace Eisenbahnsimulator {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^  dateiToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  neuToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  speichernToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  ladenToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  schließenToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  ansichtToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  hilfeToolStripMenuItem;
+	private: System::Windows::Forms::Panel^  panel1;
+	private: System::Windows::Forms::GroupBox^  groupBox1;
+
+	private: System::Windows::Forms::GroupBox^  groupBox3;
+
+	private: System::Windows::Forms::ComboBox^  comboBox2;
+
+
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TrackBar^  trackBar2;
+
+
+
+
+	private: System::Windows::Forms::RadioButton^  radioButton4;
+	private: System::Windows::Forms::RadioButton^  radioButton2;
+	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::GroupBox^  groupBox5;
+
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::ListBox^  listBox1;
+	private: System::Windows::Forms::ToolStripMenuItem^  überToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  einstellungenToolStripMenuItem;
 
 	private:
 		/// <summary>
-		/// Erforderliche Designervariable.
+		/// Required designer variable.
 		/// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// Erforderliche Methode für die Designerunterstützung.
-		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->dateiToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->neuToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->speichernToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->ladenToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->schließenToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->ansichtToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->einstellungenToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->hilfeToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->überToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->groupBox5 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
+			this->radioButton4 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton2 = (gcnew System::Windows::Forms::RadioButton());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->trackBar2 = (gcnew System::Windows::Forms::TrackBar());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->menuStrip1->SuspendLayout();
+			this->panel1->SuspendLayout();
+			this->groupBox3->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar2))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// MainFrame
+			// menuStrip1
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->dateiToolStripMenuItem,
+					this->ansichtToolStripMenuItem, this->hilfeToolStripMenuItem
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(1309, 24);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// dateiToolStripMenuItem
+			// 
+			this->dateiToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
+				this->neuToolStripMenuItem,
+					this->speichernToolStripMenuItem, this->ladenToolStripMenuItem, this->schließenToolStripMenuItem
+			});
+			this->dateiToolStripMenuItem->Name = L"dateiToolStripMenuItem";
+			this->dateiToolStripMenuItem->Size = System::Drawing::Size(46, 20);
+			this->dateiToolStripMenuItem->Text = L"Datei";
+			// 
+			// neuToolStripMenuItem
+			// 
+			this->neuToolStripMenuItem->Name = L"neuToolStripMenuItem";
+			this->neuToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->neuToolStripMenuItem->Text = L"Neu";
+			this->neuToolStripMenuItem->Click += gcnew System::EventHandler(this, &MainFrame::neuToolStripMenuItem_Click);
+			// 
+			// speichernToolStripMenuItem
+			// 
+			this->speichernToolStripMenuItem->Name = L"speichernToolStripMenuItem";
+			this->speichernToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->speichernToolStripMenuItem->Text = L"Speichern";
+			// 
+			// ladenToolStripMenuItem
+			// 
+			this->ladenToolStripMenuItem->Name = L"ladenToolStripMenuItem";
+			this->ladenToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->ladenToolStripMenuItem->Text = L"Laden";
+			// 
+			// schließenToolStripMenuItem
+			// 
+			this->schließenToolStripMenuItem->Name = L"schließenToolStripMenuItem";
+			this->schließenToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->schließenToolStripMenuItem->Text = L"Schließen";
+			this->schließenToolStripMenuItem->Click += gcnew System::EventHandler(this, &MainFrame::schließenToolStripMenuItem_Click);
+			// 
+			// ansichtToolStripMenuItem
+			// 
+			this->ansichtToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->einstellungenToolStripMenuItem });
+			this->ansichtToolStripMenuItem->Name = L"ansichtToolStripMenuItem";
+			this->ansichtToolStripMenuItem->Size = System::Drawing::Size(75, 20);
+			this->ansichtToolStripMenuItem->Text = L"Bearbeiten";
+			this->ansichtToolStripMenuItem->Click += gcnew System::EventHandler(this, &MainFrame::ansichtToolStripMenuItem_Click);
+			// 
+			// einstellungenToolStripMenuItem
+			// 
+			this->einstellungenToolStripMenuItem->Name = L"einstellungenToolStripMenuItem";
+			this->einstellungenToolStripMenuItem->Size = System::Drawing::Size(145, 22);
+			this->einstellungenToolStripMenuItem->Text = L"Einstellungen";
+			// 
+			// hilfeToolStripMenuItem
+			// 
+			this->hilfeToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->überToolStripMenuItem });
+			this->hilfeToolStripMenuItem->Name = L"hilfeToolStripMenuItem";
+			this->hilfeToolStripMenuItem->Size = System::Drawing::Size(44, 20);
+			this->hilfeToolStripMenuItem->Text = L"Hilfe";
+			// 
+			// überToolStripMenuItem
+			// 
+			this->überToolStripMenuItem->Name = L"überToolStripMenuItem";
+			this->überToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->überToolStripMenuItem->Text = L"Über...";
+			this->überToolStripMenuItem->Click += gcnew System::EventHandler(this, &MainFrame::überToolStripMenuItem_Click);
+			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->groupBox5);
+			this->panel1->Location = System::Drawing::Point(220, 28);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(1076, 661);
+			this->panel1->TabIndex = 1;
+			// 
+			// groupBox5
+			// 
+			this->groupBox5->Location = System::Drawing::Point(834, 495);
+			this->groupBox5->Name = L"groupBox5";
+			this->groupBox5->Size = System::Drawing::Size(229, 152);
+			this->groupBox5->TabIndex = 0;
+			this->groupBox5->TabStop = false;
+			this->groupBox5->Text = L"Ausgwähltes Objekt bearbeiten";
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Location = System::Drawing::Point(13, 28);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(201, 661);
+			this->groupBox1->TabIndex = 2;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Elemente";
+			// 
+			// groupBox3
+			// 
+			this->groupBox3->Controls->Add(this->radioButton4);
+			this->groupBox3->Controls->Add(this->radioButton2);
+			this->groupBox3->Controls->Add(this->button4);
+			this->groupBox3->Controls->Add(this->button2);
+			this->groupBox3->Controls->Add(this->label2);
+			this->groupBox3->Controls->Add(this->trackBar2);
+			this->groupBox3->Location = System::Drawing::Point(220, 736);
+			this->groupBox3->Name = L"groupBox3";
+			this->groupBox3->Size = System::Drawing::Size(201, 172);
+			this->groupBox3->TabIndex = 4;
+			this->groupBox3->TabStop = false;
+			this->groupBox3->Text = L"Ausgewählter Zug";
+			// 
+			// radioButton4
+			// 
+			this->radioButton4->AutoSize = true;
+			this->radioButton4->Location = System::Drawing::Point(123, 83);
+			this->radioButton4->Name = L"radioButton4";
+			this->radioButton4->Size = System::Drawing::Size(71, 17);
+			this->radioButton4->TabIndex = 6;
+			this->radioButton4->TabStop = true;
+			this->radioButton4->Text = L"rückwärts";
+			this->radioButton4->UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this->radioButton2->AutoSize = true;
+			this->radioButton2->Location = System::Drawing::Point(10, 83);
+			this->radioButton2->Name = L"radioButton2";
+			this->radioButton2->Size = System::Drawing::Size(65, 17);
+			this->radioButton2->TabIndex = 5;
+			this->radioButton2->TabStop = true;
+			this->radioButton2->Text = L"vorwärts";
+			this->radioButton2->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(10, 113);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(185, 23);
+			this->button4->TabIndex = 5;
+			this->button4->Text = L"Zug entfernen";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(9, 142);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(185, 23);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"Stop";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(6, 53);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(85, 13);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Geschwindigkeit";
+			// 
+			// trackBar2
+			// 
+			this->trackBar2->Location = System::Drawing::Point(6, 21);
+			this->trackBar2->Name = L"trackBar2";
+			this->trackBar2->Size = System::Drawing::Size(188, 45);
+			this->trackBar2->TabIndex = 1;
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Location = System::Drawing::Point(221, 705);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(200, 21);
+			this->comboBox2->TabIndex = 6;
+			this->comboBox2->Text = L"Auswahl Zug";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(428, 705);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(868, 203);
+			this->textBox1->TabIndex = 8;
+			this->textBox1->Text = L"MessageBox/Konsole";
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->Items->AddRange(gcnew cli::array< System::Object^  >(1) { L"Liste der vorhandenen Züge" });
+			this->listBox1->Location = System::Drawing::Point(13, 705);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(201, 212);
+			this->listBox1->TabIndex = 9;
+			this->listBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MainFrame::listBox1_SelectedIndexChanged);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(620, 478);
-			this->Name = L"MainFrame";
+			this->ClientSize = System::Drawing::Size(1309, 920);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->groupBox3);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
+			this->Name = L"MyForm";
 			this->ShowIcon = false;
 			this->Text = L"Eisenbahnsimulator";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->panel1->ResumeLayout(false);
+			this->groupBox3->ResumeLayout(false);
+			this->groupBox3->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar2))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
+	private: System::Void trackBar1_Scroll(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void listBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void ansichtToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void neuToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void schließenToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+		this->Close();
+	}
+	private: System::Void überToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+
+	}
 	};
 }
