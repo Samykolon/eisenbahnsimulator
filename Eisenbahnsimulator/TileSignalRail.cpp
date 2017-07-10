b6410f72e6bff46090c4c6577e6e6f483fbbce94
Vuizur <Vuizur@users.noreply.github.com>
2017-07-10 18:50:50 +0200

    Fix SignalRails

--- Eisenbahnsimulator/TileSignalRail.cpp
+++ Eisenbahnsimulator/TileSignalRail.cpp
@@ -1,8 +1,7 @@
 ﻿#include "TileSignalRail.h"
 
 
-inline SignalRail::SignalRail(Directions dir, double gdur, double rdur, String^ imagePath_1, String^ imagePath_2, String ^name) : Rail(dir, imagePath_1, name), greenDuration(gdur), redDuration(rdur){
-	timer = 0;
+inline SignalRail::SignalRail(Directions dir, String^ imagePath_1, String^ imagePath_2, String ^name) : Rail(dir, imagePath_1, name){
 	IsGreen = true;
 	imagePath1 = imagePath_1;
 	imagePath2 = imagePath_2;
